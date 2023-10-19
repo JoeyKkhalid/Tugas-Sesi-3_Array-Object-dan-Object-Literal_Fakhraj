27f8d50aa495a55f3d417c7208418b897663ce07
JoeyKkhalid <114896032+JoeyKkhalid@users.noreply.github.com>
2023-10-19 16:42:50 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,15 @@
+const restaurant = [
+    {
+      name: "Young Crew",
+      city: "Bogor",
+      favoriteDrink: "Espresso Romano (Italy)",
+      favoriteFood: "Spanakopita Triangles (Greece)",
+      isVegan: true,
+    },
+  ];
+  
+  console.log("Nama : " + restaurant[0].name);
+  console.log("Kota : " + restaurant[0].city);
+  console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+  console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+  console.log("Vegan : " + restaurant[0].isVegan);
---
+++ variable.js
@@ -0,0 +1,14 @@
+const restaurant = {
+    name: "Young Crew",
+    city: "Bogor",
+    favoriteDrink: "Espresso Romano (Italy)",
+    favoriteFood: "Spanakopita Triangles (Greece)",
+    isVegan: false,
+  };
+  
+  const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+  console.log("Nama : " + name);
+  console.log("Kota : " + city);
+  console.log("Minuman Favorit : " + favoriteDrink);
+  console.log("Makanan Favorit : " + favoriteFood);
+  console.log("Vegan : " + isVegan);
